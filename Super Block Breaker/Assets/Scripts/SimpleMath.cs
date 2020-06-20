12c1d1e030cf279e5efcba35748489b99145d478
Willians Nieves <williamsnieves@gmail.com>
2020-06-21 01:07:56 +0200

    init game

---
+++ Super Block Breaker/Assets/Scripts/SimpleMath.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMath : MonoBehaviour
+{
+    float numberChallenge = 6.2f;
+    int remainder = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        remainder = 126 % 4;
+        Debug.Log(remainder);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Super Block Breaker/Assets/Scripts/StartButtonTween.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartButtonTween : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        LeanTween.scale(gameObject, new Vector3(1.5f, 1.5f, 1.5f), 0.5f).setLoopPingPong();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+}
