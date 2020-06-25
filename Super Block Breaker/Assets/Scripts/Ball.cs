8489bed1e67be97f46cffc83db799ecc9f5aff55
Willians Nieves <williamsnieves@gmail.com>
2020-06-26 00:07:37 +0200

    added colliders

--- Super Block Breaker/Assets/Scripts/Ball.cs
+++ Super Block Breaker/Assets/Scripts/Ball.cs
@@ -5,23 +5,50 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     [SerializeField] Paddle paddle1;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 15f;
+    [SerializeField] float randomFactor = 0.2f;
     Vector2 paddleToBallVector;
+    Rigidbody2D myRigidBody2D;
+    bool hasStarted = false;
    
     // Start is called before the first frame update
     void Start()
     {
         paddleToBallVector = transform.position - paddle1.transform.position;
+        myRigidBody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //LockBallToPaddle();
+        if(!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
     }
 
     void LockBallToPaddle()
     {
         Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddlePos + paddleToBallVector;
+    }
+
+    void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Vector2 velocityTweak = new Vector2(UnityEngine.Random.Range(0, randomFactor), Random.Range(0, randomFactor));
+        
+        myRigidBody2D.velocity += velocityTweak;
     }
 }
--- Super Block Breaker/Assets/Scripts/Paddle.cs
+++ Super Block Breaker/Assets/Scripts/Paddle.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
+    [SerializeField] float screenWidthInUnits = 16f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,13 @@ public class Paddle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        var paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(GetXPos(), minX, maxX);
+        transform.position = paddlePos;
+    }
+
+    private float GetXPos()
+    {
+        return Input.mousePosition.x / Screen.width * screenWidthInUnits;
     }
 }
